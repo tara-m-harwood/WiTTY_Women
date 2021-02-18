129f5f6f37d4b86407d76837021b86221237d149
Tara Harwood <tara.m.harwood@gmail.com>
2021-02-18 14:36:39 -0800

    basic score history

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
     has_many :userquestions 
     has_many :questions, through: :userquestions
+
+    def all_tests
+        tests_taken = Test.where(user: self)
+    end
+    
+    def score_history
+        correct = 0
+        all_tests.each do |test|
+            puts "Quote: #{test.question.quote}"
+            puts "Your answer: #{test.user_answer}"
+            puts "Correct answer: #{test.question.right_answer}"
+            if test.user_answer == test.question.right_answer
+                puts ("CORRECT").green
+            else
+                puts ("INCORRECT").red
+            end
+            puts ("*****").blue
+        end
+    end               
+
 end
+
+   
+
--- runner.rb
+++ runner.rb
@@ -25,6 +25,7 @@ end
 # main runner
 welcome_banner
 current_user = gets_name
+
 menu_choice = wants_quiz
     if menu_choice == "Exit"
         puts "Goodbye! See you next time!"
@@ -34,18 +35,22 @@ menu_choice = wants_quiz
         for i in 1..3
             current_question = Question.random(already_asked)
             already_asked << current_question
-            binding.pry
             user_test = Test.create user: current_user, question: current_question
             puts ("*********").blue
             puts ("'#{user_test.question.quote}'").yellow
             user_choice = prompt.select("Who wrote it?",user_test.question.choices)
+            user_test.update(user_answer: user_choice)
             puts ("*********").blue
             if user_choice == user_test.question.right_answer
                 puts ("CORRECT! This is a quote by #{user_test.question.right_answer}!").green
             else
                 puts ("Sorry! This was actually written by #{user_test.question.right_answer}!").red
             end        
-        end    
+        end
+        # score_history = prompt.select("Do you want to see your score history?", ["Yes","No"])
+        #     if score_history = "Yes"
+        #         show_scores 
+
     end    
 
 
