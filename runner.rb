e7332b896c2abaea35cfa6d85cb0207495b86582
Tara Harwood <tara.m.harwood@gmail.com>
2021-02-18 12:35:56 -0800

    first run at runner

--- runner.rb
+++ runner.rb
@@ -8,6 +8,7 @@ end
 def welcome_banner
     banner = Artii::Base.new(:font => "slant")
     puts banner.asciify("WiTTY Women").blue
+    puts ("When you think about legendary writers, are you missing half the story?").blue
 end
 
 def gets_name
@@ -16,14 +17,31 @@ def gets_name
     puts "Welcome, #{user_name}! Would you like to test your literary knowledge?"
 end
 
+def wants_quiz
+    prompt.select("Select an option:", ["Yes, show me a quote!","Exit"])
+end    
 
 # main runner
 welcome_banner
 gets_name
-user_question = Question.random
-puts user_question.quote
-user_choice = prompt.select("Who wrote it?",user_question.choices)
-puts "The correct answer is #{user_question.right_answer}"
+menu_choice = wants_quiz
+    if menu_choice == "Exit"
+        puts "Goodbye! See you next time!"
+        exit
+    else    
+        for i in 1..3
+            user_question = Question.random
+            puts ("*********").blue
+            puts ("'#{user_question.quote}'").yellow
+            user_choice = prompt.select("Who wrote it?",user_question.choices)
+            puts ("*********").blue
+            if user_choice == user_question.right_answer
+                puts ("CORRECT! This is a quote by #{user_question.right_answer}!").green
+            else
+                puts ("Sorry! This was actually written by #{user_question.right_answer}!").red
+            end        
+        end    
+    end    
 
 
 #binding.pry
